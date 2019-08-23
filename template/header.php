9122a498986e6b041fea7ef58944979cd96360a6
krewaz234 <53952343+krewaz234@users.noreply.github.com>
2019-08-23 14:20:04 +0300

    Добавлены классы для формы авторизации

--- template/header.php
+++ template/header.php
@@ -3,9 +3,9 @@
 <meta name="keywords" content="программирование, олимпиады, олимпиадное программирование, алгритмы">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
 integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="template/main.js"></script>
 <title>Мир олимпиадного программирования</title>
 <style media="screen">
-
 html, body {
   margin: 0;
   padding: 0;
@@ -13,6 +13,68 @@ html, body {
   font: 400 1.2em Arial;
   background: #111116;
 }
+
+.link {
+  font-size: 2em;
+  font-weight: 1000;
+  margin-left: 5%;
+}
+
+.authorised
+{
+  text-align: center;
+  font-size: 0.7em;
+  letter-spacing: 0;
+  color: white;
+  font-weight: 400;
+  text-decoration: none;
+  transition: 0.5s;
+}
+
+.authorised:hover
+{
+  text-decoration: none;
+  color: #999;
+}
+
+#authorisewindow
+{
+  position: absolute;
+  top: -350px;
+  padding: 30px;
+  right: 0px;
+  height: 350px;
+  width: 300px;
+  background: rgb(255, 255, 255);
+  z-index: 99999;
+  background: #222226;
+  transition: 0.5s;
+}
+
+.authinput
+{
+  background: rgb(60,60,60);
+  border: none;
+  width: 250px;
+  color: white;
+  font-size: 0.7em;
+  height: 2em;
+  line-height: 6em;
+  font-weight: 600;
+  padding: 5px;
+}
+
+.authbutton
+{
+  background: rgb(60,60,60);
+  width: 80px;
+  height: 35px;
+  color: white;
+  font-size: 0.7em;
+  font-weight: 600;
+  border: none;
+}
+
 a {
   color: white;
   transition: 0.5s;
@@ -23,14 +85,31 @@ a:hover {
   text-decoration: none;
 }
 
-.link {
-  font-size: 2em;
-  font-weight: 1000;
-  margin-left: 5%;
+.smalltext
+{
+  width: 100%;
+  text-align: center;
+  color: white;
+  font-size: 0.7em;
+  line-height: 1.2em;
+}
+
+.smalllink
+{
+  color: inherit;
+  text-decoration: none;
+  font-size: inherit;
+  transition: 0.5s;
+}
+
+.smalllink:hover {
+  color: #999;
+  text-decoration: none;
 }
 
 .header {
   background: #222;
+  z-index: 9999999;
 }
 
 .context {
@@ -40,12 +119,22 @@ a:hover {
 .main {
   min-height: 70vh;
   background: #222226;
+  z-index: 9999;
+}
+
+.sidebar, .sidebar-2 {
+  margin-left: 1%;
+  background: #222;
+  z-index: 9999;
 }
 
 .sidebar {
   margin-left: 1%;
   min-height: 40vh;
-  background: #222;
+}
+
+.sidebar-2 {
+  min-height: 40vh;
 }
 
 .footer {
