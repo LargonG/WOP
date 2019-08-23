969bea40365ce07a3d362f74407cc148c6148b24
LargonG <44260678+LargonG@users.noreply.github.com>
2019-08-23 22:33:14 +1100

    Create header-content.php

---
+++ texts/header-content.php
@@ -0,0 +1,24 @@
+<style media="screen">
+  .authorised
+  {
+    text-align: center;
+    font-size: 0.7em;
+    letter-spacing: 0;
+    color: white;
+    font-weight: 400;
+    text-decoration: none;
+    transition: 0.5s;
+  }
+  .authorised:hover
+  {
+    text-decoration: none;
+    color: #999;
+  }
+</style>
+
+<a href="index.php" class="navbar-item link">
+     WOP
+  </a>
+<a href="#" class="navbar-item authorised" onclick="authorise_roll()">
+   Вы&nbspиспользуете&nbspгостевой доступ<br>Авторизуйтесь!
+</a>
