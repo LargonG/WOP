a9263e5eee585e676ac1f71d66382de10c79c842
LargonG <44260678+LargonG@users.noreply.github.com>
2019-08-23 22:02:53 +1100

    Create nav.php

---
+++ template/nav.php
@@ -0,0 +1,9 @@
+<ul style="margin-top: 5vh;
+padding-left: 0;
+list-style-type: none;
+font-size: 1.5em;">
+  <li style="margin-top:0.5vh;"><a href="algorithms.php">Алгоритмы</a></li>
+  <li style="margin-top:0.5vh;"><a href="#">Задачи</a></li>
+  <li style="margin-top:0.5vh;"><a href="#">Контесты</a></li>
+  <li style="margin-top:0.5vh;"><a href="#">Досуг</a></li>
+</ul>
