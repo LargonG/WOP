40d99553b119677e216230bb400daa4ce909a9fb
LargonG <44260678+LargonG@users.noreply.github.com>
2019-08-23 20:51:56 +1100

    Изменены стили

--- template/header.php
+++ template/header.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="keywords" content="программирование, олимпиады, одимпиадное программирование, алгритмы">
+<meta name="keywords" content="программирование, олимпиады, олимпиадное программирование, алгритмы">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
 integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <title>Мир олимпиадного программирования</title>
@@ -9,12 +9,24 @@ html, body {
   margin: 0;
   padding: 0;
   color: white;
-  font: 400 1.9em Arial;
+  font: 400 1.2em Arial;
   background: #111116;
 }
 
+.link {
+  color: white;
+  transition: 0.5s;
+  font-size: 1.5em;
+  font-weight: 1000;
+  margin-left: 5%;
+}
+
+.link:hover {
+  color: #999;
+  text-decoration: none;
+}
+
 .header {
-  min-height: 7vh;
   background: #222;
 }
 
@@ -27,10 +39,17 @@ html, body {
   background: #222226;
 }
 
-.sidebar {
+.sidebar, .sidebar-2 {
   margin-left: 1%;
-  min-height: 70vh;
   background: #222;
+}
+
+.sidebar {
+  min-height: 40vh;
+}
+
+.sidebar-2 {
+  min-height: 40vh;
 }
 
 .footer {
