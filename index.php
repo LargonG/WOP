44b3215cbbff8064c430b58fc987b24bf3fa2f11
LargonG <44260678+LargonG@users.noreply.github.com>
2019-08-23 20:09:43 +1100

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <?php require 'header.php' ?>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col header">
+          HEADER
+        </div>
+      </div>
+    </div>
+    <div class="container context">
+      <div class="row">
+        <div class="col-8 main">
+          CONTENT
+        </div>
+        <div class="col sidebar">
+          SIDEBAR
+        </div>
+      </div>
+    </div>
+    <div class="container footer">
+      <div class="row">
+        <div class="col">
+          FOOTER
+        </div>
+      </div>
+    </div>
+    <?php require "jQueryScripts.php" ?>
+  </body>
+</html>
