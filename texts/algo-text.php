a4f948a3cc0f0251791416d16dc48d59556824d2
LargonG <44260678+LargonG@users.noreply.github.com>
2019-08-23 23:24:35 +1100

    Update algo-text.php

--- texts/algo-text.php
+++ texts/algo-text.php
@@ -1,1 +1,11 @@
-<b>Ну что же, приступим</b>
+<div class="row">
+  <div class="col-4" style="overflow:hidden">
+    <img src="/imgs/graph.png" alt="" style="height: 100%;">
+  </div>
+  <div class="col-4">
+
+  </div>
+  <div class="col-4">
+
+  </div>
+</div>
