7aa2736f4b24386f3e89a955c2601b0b316aec44
LargonG <44260678+LargonG@users.noreply.github.com>
2019-08-23 22:04:14 +1100

    Create algorithms.php

---
+++ algorithms.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <?php require 'template/header.php' ?>
+  </head>
+  <body>
+    <div class="navbar header">
+      <a href="index.php" class="navbar-item link">
+         WOP
+      </a>
+    </div>
+    <div class="container context">
+      <div class="row">
+        <div class="col-8 main">
+          <?php require "texts/algo-text.php" ?>
+        </div>
+        <div class="col sidebar">
+          <?php require "template/nav.php" ?>
+        </div>
+      </div>
+    </div>
+    <div class="container footer">
+      <div class="row">
+        <div class="col">
+          FOOTER
+        </div>
+      </div>
+    </div>
+    <?php require "template/jQueryScripts.php" ?>
+  </body>
+</html>
