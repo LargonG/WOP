647a6881907ca3c5bf297c7e6524a8fb4a5fe8b6
LargonG <44260678+LargonG@users.noreply.github.com>
2019-08-23 20:11:26 +1100

    Create header.php

---
+++ template/header.php
@@ -0,0 +1,41 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="keywords" content="программирование, олимпиады, одимпиадное программирование, алгритмы">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<title>Мир олимпиадного программирования</title>
+<style media="screen">
+html, body {
+  margin: 0;
+  padding: 0;
+  color: white;
+  font: 400 1.9em Arial;
+  background: #111116;
+}
+
+.header {
+  min-height: 7vh;
+  background: #222;
+}
+
+.context {
+  margin-top: 1vh;
+}
+
+.main {
+  min-height: 70vh;
+  background: #222226;
+}
+
+.sidebar {
+  margin-left: 1%;
+  min-height: 70vh;
+  background: #222;
+}
+
+.footer {
+  margin-top: 1vh;
+  min-height: 21vh;
+  background: #222;
+}
+</style>
