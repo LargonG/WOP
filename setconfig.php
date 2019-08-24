9a7558d8c4c6fa5a1702a5cffd06b9c91a136432
krewaz234 <53952343+krewaz234@users.noreply.github.com>
2019-08-24 12:54:27 +0300

    Update setconfig.php

--- setconfig.php
+++ setconfig.php
@@ -1,7 +1,7 @@
 <?php
   require $_SERVER['DOCUMENT_ROOT'].'/template/login.php';
   if(entered()) {
-    setcookie('name', $_POST['usname'], time() + (86400 * 30));
+    setcookie('name', $_POST['usname'], time() + 3600*2);
   }
   header("Refresh:0; url=index.php");
 ?>
