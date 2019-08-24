c263a84cad282470689c8ad4b16de9fea1873953
LargonG <44260678+LargonG@users.noreply.github.com>
2019-08-24 15:06:37 +1100

    Лучше сюда
    В texts должны находиться только фалы, которые будут в дальнейшем изменяться  людьми, которые пишут какие-то статьи

---
+++ template/header-content.php
@@ -0,0 +1,25 @@
+<style media="screen">
+  .authorised
+  {
+    text-align: center;
+    font-size: 0.7em;
+    letter-spacing: 0;
+    color: white;
+    font-weight: 400;
+    text-decoration: none;
+    transition: 0.5s;
+  }
+  .authorised:hover
+  {
+    text-decoration: none;
+    color: #999;
+    cursor: pointer;
+  }
+</style>
+
+<a href="http://worldofop.ru" class="navbar-item link">
+     WOP
+  </a>
+<div href="#" class="navbar-item authorised" onclick="authorise_roll()">
+   Вы&nbspиспользуете&nbspгостевой доступ<br>Авторизуйтесь!
+</div>
