241a6b26feca75844dfb508cb67610e5e54609d1
krewaz234 <53952343+krewaz234@users.noreply.github.com>
2019-08-27 12:28:47 +0300

    Логин через емейл

--- setconfig.php
+++ setconfig.php
@@ -5,7 +5,10 @@
       if (!$user)
           return false;
       if (password_verify($_POST['pass'], $user->password))
+      {
+          $login = $user->username;
           return true;
+      }
       else
           return false;
   }
@@ -25,10 +28,19 @@
             return checkpass($user);
       }
   }
+
+  function userlogin()
+  {
+    $user = R::findOne('userlogindata', 'username = ?', array($_POST['usname']));
+    if (!$user)
+      return R::findOne('userlogindata', 'email = ?', array($_POST['usname']))->username;
+    return $user->username;
+  }
+
   if(entered()) {
-    setcookie('name', $_POST['usname'], time() + (86400 * 30), '/'); //тебе не кажется, что 30 дней жирновато?
+    setcookie('name', userlogin(), time() + (86400 * 30), '/'); //тебе не кажется, что 30 дней жирновато?
     setcookie('logsuccess', 1, time() + (86400 * 30), '/');
-    setcookie('lastenteredlogin', $_POST['usname'], time() + (86400 * 30), '/');
+    setcookie('lastenteredlogin', userlogin(), time() + (86400 * 30), '/');
   }
   else
   {
