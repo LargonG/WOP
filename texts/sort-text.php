da60c5924ab42c8a1630cf4cb48da81d0809590f
LargonG <44260678+LargonG@users.noreply.github.com>
2019-08-30 19:25:14 +1100

    Изменён цвет кода в светлой теме

--- texts/sort-text.php
+++ texts/sort-text.php
@@ -13,14 +13,26 @@
     box-sizing: border-box;
     border-top: 1px solid #333336;
     border-bottom: 1px solid #333336;
+    color: orange;
   }
 
   .code code {
     white-space: pre-wrap;
-    color: orange;
+    color: inherit;
   }
 
 </style>
+
+<?php if (isset($_COOKIE['lighttheme'])): ?>
+  <style media="screen">
+    .code {
+      background: #d4d4d9;
+      border-top-color: #ddd;
+      border-bottom-color: #ddd;
+      color: #343434;
+    }
+  </style>
+<?php endif ?>
 <div class="text">
   <h2>Сортировка</h2>
   <p>Перед тем, как рассуждать, как сделать сортировку правильно и быстро, мы должны сначала понять, что это вообще такое. <br>
