c9ae00895741006fa172334e426f39328b7a55a7
LargonG <LargonG@users.noreply.github.com>
2019-12-11 16:57:33 +1100

    Статья про сортировку теперь работает
    Пофиксил баги, связанные с переделкой внешнего вида сайта.

--- papers/sort/index.php
+++ papers/sort/index.php
@@ -5,26 +5,14 @@
     <?php require $_SERVER['DOCUMENT_ROOT']."/papers/papers-style.php" ?>
   </head>
   <body>
-    <div class="navbar header">
-      <?php require $_SERVER['DOCUMENT_ROOT']."/template/header-content.php" ?>
-    </div>
-    <?php require $_SERVER['DOCUMENT_ROOT'].'/template/singin.php' ?>
-    <div class="container-fluid context">
-      <div class="row">
-        <div class="col">
-
-        </div>
-        <div class="col-xl-9 col-12 main">
-          <?php include $_SERVER['DOCUMENT_ROOT']."/texts/sort-text.php" ?>
-        </div>
-        <div class="col-xl-2 col sidebar">
-          <?php require $_SERVER['DOCUMENT_ROOT']."/template/nav.php" ?>
-        </div>
-      </div>
-    </div>
-    <div class="col-12 footer">
-      <?php require $_SERVER['DOCUMENT_ROOT']."/template/footer-content.php"; ?>
-    </div>
+    <?php require $_SERVER['DOCUMENT_ROOT']."/template/navbar.php" ?>
+    <?php require $_SERVER['DOCUMENT_ROOT']."/template/main.php" ?>
+    <?php require $_SERVER['DOCUMENT_ROOT']."/template/footer.php" ?>
     <?php require $_SERVER['DOCUMENT_ROOT'].'/template/jQueryScripts.php' ?>
+    <!-- <script type="text/javascript">
+      $(".context").removeClass("container");
+      $(".context").addClass("container-fluid");
+      alert("YES");
+    </script> -->
   </body>
 </html>
