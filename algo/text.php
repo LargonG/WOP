3fafe1d9bd886bc70cad2beb3e0b54871183c086
LargonG <LargonG@users.noreply.github.com>
2020-01-12 18:23:35 +0300

    Добавил статью про сортировку слиянием

--- algo/text.php
+++ algo/text.php
@@ -1,5 +1,6 @@
 <ul>
-  <li><a class="text-light" href="/papers/bubble-sort">Сортировка</a> </li>
+  <li><a class="text-light" href="/papers/bubble-sort">Сортировка пузырьком</a> </li>
+  <li><a class="text-light" href="/papers/merge-sort">Сортировка слиянием</a> </li>
 </ul>
 <script type="text/javascript">
   $('.link_algo').addClass('active');
