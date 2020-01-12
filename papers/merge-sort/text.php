efd8dc550e1db2255e0f5ff965831a917426aeb2
LargonG <LargonG@users.noreply.github.com>
2020-01-12 18:25:50 +0300

    Добавил merge-sort

---
+++ papers/merge-sort/text.php
@@ -0,0 +1,29 @@
+<div class="text">
+  <h2>Сортировка слиянием</h2>
+  <p>Сортировка слиянием - одна из самых быстрых и стабильных сортировок без задания дополнительных условий</p>
+  <h4>Алгоритм</h4>
+  <p>Разделим массив на две равные части. Далее рекурсивно отсортируем его. Далее, при слиянии двух частей</p>
+  <h4>Реализация</h4>
+  <div class="code">
+    <code>
+#include &ltbits/stdc++.h&gt
+
+using namespace std;
+
+vector&ltint&gt merge_sort() {
+  
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector&ltint&gt v(n);
+  for (auto& t : v)
+    cin >> t;
+
+  return 0;
+}
+    </code>
+  </div>
+
+</div>
