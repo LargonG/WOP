76c5e24f01d7720c0bc139f3f207436a3ec23db7
LargonG <LargonG@users.noreply.github.com>
2020-08-02 01:34:13 +1100

    Исправлена страница 404
    Читаемый текст

--- err404.php
+++ err404.php
@@ -2,28 +2,16 @@
 <html lang="ru" dir="ltr">
   <head>
     <?php require $_SERVER['DOCUMENT_ROOT']."/template/head.html"; ?>
-    <style media="screen">
-      .text-content {
-        text-align: center;
-      }
-      .main {
-        margin-top: 5vh;
-        padding: 15px;
-      }
-    </style>
+    <title>Ошибка 404 | Мир олимпиадного программирования</title>
   </head>
   <body>
     <?php require $_SERVER['DOCUMENT_ROOT'].'/template/navbar.php' ?>
-    <div class="container main">
-      <div class="row">
-        <div class="col-12 text-content">
-          <h1>Ошибка 404</h1>
-          <span style="font-style: italic; color: #939393">Страница не найдена.</span>
-        </div>
-        <div class="col text-content" style="margin-top:5vh; font-size: 1.2em">
-          Если вы зашли на данную страницу, значит файл, который вы искали не существует. Рекомендуем вернуться на <a href="/" style="font-weight: 700">главную страницу</a>.
-        </div>
+    <div class="container main mt-5">
+      <div class="h1 text-light text-center">Ошибка 404</div>
+      <div class="h4 text-weight-normal text-center text-light">
+      Если вы зашли на данную страницу, значит страница, которую вы искали, не существует. Рекомендуем вернуться на <a href="/" style="font-weight: 700">главную страницу</a>.
       </div>
     </div>
+    <?php require $_SERVER['DOCUMENT_ROOT'].'/template/jQueryScripts.html'; ?>
   </body>
 </html>
