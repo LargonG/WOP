2f6503b8e3f6a77db5c1791940a5f168d752ca57
LargonG <LargonG@users.noreply.github.com>
2020-08-01 22:19:41 +1100

    Relogic of algo

--- algo/index.php
+++ algo/index.php
@@ -10,5 +10,8 @@
     <?php require $_SERVER['DOCUMENT_ROOT']."/template/main.php" ?>
     <?php require $_SERVER['DOCUMENT_ROOT']."/template/footer.php" ?>
     <?php require $_SERVER['DOCUMENT_ROOT'].'/template/jQueryScripts.php' ?>
+    <script type="text/javascript">
+      $('.link_algo').addClass('active');
+    </script>
   </body>
 </html>
--- algo/text.php
+++ algo/text.php
@@ -2,6 +2,3 @@
   <li><a class="text-light" href="/papers/bubble-sort">Сортировка пузырьком</a> </li>
   <li><a class="text-light" href="/papers/merge-sort">Сортировка слиянием</a> </li>
 </ul>
-<script type="text/javascript">
-  $('.link_algo').addClass('active');
-</script>
