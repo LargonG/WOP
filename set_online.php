9abde03fc5cc104637985d0024082b97ca663c58
Michael Korobkov <korobkov200364@gmail.com>
2020-08-01 20:43:32 +0300

    Bruh, еще один фикс коммита 42ea0ad

--- set_online.php
+++ set_online.php
@@ -3,6 +3,8 @@ require $_SERVER['DOCUMENT_ROOT']."/database/dbase.php";
 if (isset($_COOKIE['name']))
 {
     $user = R::findOne('userlogindata', 'username = ?', array($_COOKIE['name']));
-    R::findOne('profiledata', 'id = ?', array($user->id))->last_active = time();
+    $user = R::findOne('profiledata', 'id = ?', array($user->id));
+    $user->last_active = time();
+    R::store($user);
 }
 ?>
