efaace904dda0cc4346a1e5135247c33b60a6885
LargonG <LargonG@users.noreply.github.com>
2020-08-02 01:14:57 +1100

    Исправлен баг с 1 пикселем
    УРАААААААААААААААААААААААААа

--- home/index.php
+++ home/index.php
@@ -7,13 +7,15 @@
 </head>
 <body>
     <?php require $_SERVER['DOCUMENT_ROOT']."/template/navbar.php"; ?>
-    <div class="container context mt-2 p-3 bg-dark text-light">
+
+    <div class="container context mt-2 bg-dark text-light">
         <div class="row">
+
             <div class="col-2">
                 <img src="/imgs/kitten.png" class="rounded mx-auto d-block img-fluid" alt="">
             </div>
-            <div class="col-10">
 
+            <div class="col-10">
                 <div class="nickname h5">
                     $$$KittenProCoder3000$$$
                     <img src="/imgs/online.png" alt="">
@@ -26,16 +28,16 @@
                 <div class="email h6 font-italic font-weight-normal text-secondary">
                     kitten@catmail.catland
                 </div>
-                
             </div>
             
-            <div class="col mt-4 stats pt-3">
+            <div class="col-12 mt-4 stats pt-3">
                 <div class="col-12 text-light h3">Stats</div>
             </div>
             
             <div class="col-12 mt-2 pt-3 achievements">
                 <div class="col-12 text-light h3">Achievements</div>
             </div>
+            
         </div>
     </div>
 
