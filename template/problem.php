5db6b2e8e6af35ed97b2964194d435f856bb4f1f
LargonG <LargonG@users.noreply.github.com>
2020-08-05 18:59:40 +1100

    Улучшено отображение таблицы с тестом
    Убраны лишние барьеры

--- template/problem.php
+++ template/problem.php
@@ -1,7 +1,9 @@
 <div class="container context mt-2">
     <div class="row">
         <div class="col-12 main bg-dark text-light p-0">
-            <?php require "text.html" ?>
+            <div class="text">
+                <?php require "text.html" ?>
+            </div>
             <?php require $_SERVER["DOCUMENT_ROOT"]."/template/submitTask.php"; ?>
         </div>
     </div>
