d2f936d41177bad31c7289c25b050d53754c76d2
LargonG <LargonG@users.noreply.github.com>
2020-08-06 01:58:33 +1100

    Совсем забыл убрать лишние стили

--- signup/index.php
+++ signup/index.php
@@ -4,72 +4,13 @@
   <head>
     <?php require $_SERVER['DOCUMENT_ROOT'].'/template/head.html' ?>
     <title>Создание профиля</title>
-
-    <!-- <style media="screen">
-        .authorised
-    {
-    text-align: center;
-    font-size: 0.7em;
-    letter-spacing: 0;
-    color: white;
-    font-weight: 400;
-    text-decoration: none;
-    transition: 0.5s;
-  }
-  .authorised:hover
-  {
-    text-decoration: none;
-    color: #999;
-  }
-  h2
-  {
-    margin-top: 1vh;
-  }
-  .authinput
-  {
-    background: rgb(60,60,60);
-    border: none;
-    color: white;
-    font-size: 1em;
-    height: 2em;
-    font-weight: 600;
-    padding: 5px;
-    margin-left: 5vw;
-    border-radius: 10px;
-    margin-right: 10px;
-  }
-  .authbutton
-  {
-    background: rgb(60,60,60);
-    color: white;
-    font-size: 1em;
-    font-weight: 600;
-    border: none;
-    border-radius: 10px;
-    margin-left: 7vw;
-    padding: 0.3em;
-  }
-  .qm
-  {
-    cursor: pointer;
-    margin-left: 7px;
-  }
-  .smtext
-  {
-    transition: 0.5s;
-    font-size: 0.7em;
-  }
-  textarea:focus, input:focus{
-outline: none;
-}
-  </style> -->
   </head>
   <body>
   <?php require $_SERVER['DOCUMENT_ROOT']."/template/navbar.php" ?>
     <div class="container context bg-dark text-light mt-2">
       <div class="row">
         <div class="col-12 p-3">
-          
+
           <div class="h2" align="center">Создание профиля</div>
           <div class="h5" align="center">Маленькое начало большого пути</div>
 
