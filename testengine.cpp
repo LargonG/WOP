c01c99f6107a64240e58bb97507c5747d4856790
Michael Korobkov <korobkov200364@gmail.com>
2020-08-08 22:39:48 +0300

    Подгрузка данных по TL ML + оптимизация файлового открытия для асинхронной работы

--- testengine.cpp
+++ testengine.cpp
@@ -8,13 +8,12 @@
 using namespace std;
 
 //константы передаются из каких то файлов
-const size_t time_limit = 1000;
-const size_t maxmem = 64*1024*1024;//64 мбайта
+size_t time_limit;
+size_t maxmem;//64 мбайта
+size_t test_sets;
 string submit_path;
 string test_path;
 string language;
-char *working_path[256];
-
 
 bool test_running = false;
 
@@ -36,6 +35,9 @@ vector<string> split(string str, string delimiter)
 void load_config()
 {
     ifstream fin("testengine.cfg");
+    while (!fin.is_open()) fin.open("testengine.cfg");
+
+    //информация для компиляции/тестов
     string task_id, submit_id, lang;
     while (!fin.eof())
     {
@@ -61,25 +63,42 @@ void load_config()
     submit_path = "submits/"+task_id+"/"+submit_id;
     test_path = "tests/"+task_id;
     language = lang;
-}
 
-size_t count_test_sets()
-{
-    size_t k = 1;
-    fstream f(test_path+"/in"+to_string(k)+".txt");
-    while (f.is_open())
+    //информация по TL, ML, кол-ву тестовых наборов
+    fin.open(test_path+"/problem.cfg");
+    while (!fin.is_open()) fin.open(test_path+"/problem.cfg");
+
+    while (!fin.eof())
     {
-        f.close();
-        f.open(test_path+"/in"+to_string(++k)+".txt");
+        string line;
+        getline(fin, line);
+
+        if (line == "")
+            break;
+
+        string param = split(line, ":")[0], value = split(line, ":")[1];
+
+        while (value[0] == ' ') value.erase(0, 1);
+
+        if (param == "TL")
+            time_limit = atoi(value.c_str());
+
+        if (param == "ML")
+            maxmem = atoi(value.c_str());
+
+        if (param == "test_sets")
+            test_sets = atoi(value.c_str());
     }
-    return k-1;
+    fin.close();
 }
 
 void run_test(size_t num)
 {
     test_running = true;
-    system(("\""+submit_path+"/prog.exe\" <"+test_path+"/in"+to_string(num)+".txt >"+submit_path+"/out.txt").c_str());
+    SetCurrentDirectory(submit_path.c_str());
+    system(("prog <../../../"+test_path+"/in"+to_string(num)+".txt >../../../"+submit_path+"/out.txt").c_str());
     test_running = false;
+    SetCurrentDirectory("../../../");
 }
 
 int main()
@@ -96,12 +115,9 @@ int main()
         system("pause");
         return 0;
     }
-    cout << "Compilation success\n";
 
-    size_t test_sets = count_test_sets();
     for (int i = 1; i <= test_sets; ++i)
     {
-        cout << i << endl;
         thread th(run_test, i);//выполнение на 1 тестовом наборе
         while (!test_running){}
         //system("pause");
@@ -118,16 +134,20 @@ int main()
                 break;
             }
             //мониторинг памяти
-            system("@echo off for /f %a in ('wmic process where \"name=\'prog.exe\'\" get WorkingSetSize^|findstr [0-9]\') do (set \"var=%a\" echo %a >curmem.txt)");
-            ifstream fin("curmem.txt");
-            size_t memory;
-            fin >> memory;
-            fin.close();
-            //cout << "Memory=" << memory / 1024 << " kbytes\n";
-            if (memory > maxmem)
+            system("for /f %a in ('wmic process where \"name=\'prog.exe\'\" get WorkingSetSize^|findstr [0-9]\') do echo %a >curmem.txt");
+            ifstream fin(submit_path+"curmem.txt");
+            if (fin.is_open())
             {
-                ML = true;
-                break;
+                size_t memory;
+                fin >> memory;
+                fin.close();
+            //cout << "Memory=" << memory / 1024 << " kbytes\n";
+                if (memory > maxmem)
+                {
+                    ML = true;
+                    break;
+                }
+                fin.close();
             }
         }
         system("taskkill /f /IM prog.exe");
@@ -136,18 +156,20 @@ int main()
         if (TL)
         {
             fout << "TL";
-            cout << "test " << i << " TL\n";
+            //cout << "test " << i << " TL\n";
             break;
         }
         else if (ML)
         {
             fout << "ML";
-            cout << "test " << i << " ML\n";
+            //cout << "test " << i << " ML\n";
             break;
         }
         else
         {
-            system(("fc "+submit_path+"/out.txt "+test_path+"/ans"+to_string(i)+".txt >NUL && echo OK >"+submit_path+"/comp.txt || echo WA >"+submit_path+"/comp.txt").c_str());
+            SetCurrentDirectory(submit_path.c_str());
+            system(("fc out.txt ../../../"+test_path+"/ans"+to_string(i)+".txt >NUL && echo OK >"+"comp.txt || echo WA >"+"comp.txt").c_str());
+            SetCurrentDirectory("../../../");
             ifstream checkans(submit_path+"/comp.txt");
             string status;
             checkans >> status;
@@ -155,18 +177,16 @@ int main()
             if (status == "OK")
             {
                 fout << "OK";
-                cout << "test " << i << "OK\n";
+                //cout << "test " << i << "OK\n";
             }
             else
             {
                 fout << "WA";
-                cout << "test " << i << "WA\n";
+                //cout << "test " << i << "WA\n";
                 break;
             }
         }
         fout.close();
     }
-    cout << "here";
-    system("pause");
     return 0;
 }
