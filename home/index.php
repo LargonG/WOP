f2a9e9ea51aba45ad1fd16d08346bc20440d2156
Michael Korobkov <korobkov200364@gmail.com>
2020-08-10 19:52:10 +0300

    Смена аватара

--- home/index.php
+++ home/index.php
@@ -68,23 +68,75 @@
                 <div class="row">
 
                     <!-- Картинка -->
+                    <?php
+                    if (isset($_POST['sub']) and $_FILES['avatar']['name'] != '')
+                    {
+                        $av_extenstions = array("jpg", "png", "gif");
+                        foreach($av_extenstions as $i)
+                        {
+                            if (file_exists($_SERVER['DOCUMENT_ROOT']."/avatars/$user_userlogindata->id.$i"))
+                            {
+                                unlink($_SERVER['DOCUMENT_ROOT']."/avatars/$user_userlogindata->id.$i");
+                                break;
+                            }
+                        }
+                        $avatar_extension = explode(".", $_FILES['avatar']['name'])[count(explode(".", $_FILES['avatar']['name'])) - 1];
+                        if (move_uploaded_file($_FILES['avatar']['tmp_name'], $_SERVER['DOCUMENT_ROOT']."/avatars/$user_userlogindata->id.$avatar_extension"))
+                        {
+                            //echo "File uploaded";
+                        }
+                        else
+                        {
+                            //echo "No file";
+                        }
+
+                        //rename($_SERVER['DOCUMENT_ROOT']."/avatars/".$_FILES['uploadFile']['name'], $_SERVER['DOCUMENT_ROOT']."/avatars/$user_userlogindata->id.$avatar_extension");
+
+                        //обрезка до квадрата относительно центра
+                        $im = null;
+                        if ($avatar_extension == "jpg" or $avatar_extension == "jpeg")
+                            $im = imagecreatefromjpeg($_SERVER['DOCUMENT_ROOT']."/avatars/$user_userlogindata->id.$avatar_extension");
+                        if ($avatar_extension == "png")
+                            $im = imagecreatefrompng($_SERVER['DOCUMENT_ROOT']."/avatars/$user_userlogindata->id.$avatar_extension");
+                        if ($avatar_extension == "gif")
+                            $im = imagecreatefromgif($_SERVER['DOCUMENT_ROOT']."/avatars/$user_userlogindata->id.$avatar_extension");
+                        $size = min(imagesx($im), imagesy($im));
+                        $x = (imagesx($im) - $size) / 2;
+                        $y = (imagesy($im) - $size) / 2;
+                        $im2 = imagecrop($im, ['x' => $x, 'y' => $y, 'width' => $size, 'height' => $size]);
+                        if ($im2 !== FALSE)
+                        {
+                            imagepng($im2, $_SERVER['DOCUMENT_ROOT']."/avatars/$user_userlogindata->id.$avatar_extension");
+                            imagedestroy($im2);
+                        }
+                        imagedestroy($im);
+                    }
+                    ?>
                     <div class="col-12">
-                        <img src=<?php $path = $_SERVER['DOCUMENT_ROOT']."/avatars/".$nickname.'.jpg'; if (file_exists($path)) echo "\""."/avatars/".$nickname.".jpg"."\""; else echo "/imgs/default_avatar.jpg"; ?>
+                        <img src=
+                        <?php
+                        if (file_exists($_SERVER['DOCUMENT_ROOT']."/avatars/".$user_userlogindata->id.".jpg"))
+                            echo "\""."/avatars/".$user_userlogindata->id.".jpg"."\"";
+                        else if (file_exists($_SERVER['DOCUMENT_ROOT']."/avatars/".$user_userlogindata->id.".png"))
+                            echo "\""."/avatars/".$user_userlogindata->id.".png"."\"";
+                        else if (file_exists($_SERVER['DOCUMENT_ROOT']."/avatars/".$user_userlogindata->id.".gif")) 
+                            echo "\""."/avatars/".$user_userlogindata->id.".gif"."\"";
+                        else echo "/imgs/default_avatar.jpg"; ?>
                         class="rounded mx-auto d-block img-fluid" alt="аватарка">
                     </div>
 
                     <!-- Возможность поменять картинку -->
                     <div class="col-12 mt-2">
-                        <form class="form-group" action="">
+                        <form class="form-group" enctype="multipart/form-data" action="" method="POST">
                             
                         <!-- Ввод файла-->
                             <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="avatarFile" accept=".png,.jpg,.gif">
+                                <input type="file" name="avatar" class="custom-file-input" id="avatarFile" accept=".png,.jpg,.gif">
                                 <label class="custom-file-label" for="avatarFile">Сменить</label>
                             </div>
                             
                             <!-- Кнопка "Изменить" -->
-                            <button class="btn btn-primary form-control mt-1" type="submit">Изменить</button>
+                            <button class="btn btn-primary form-control mt-1" name="sub" type="submit">Изменить</button>
                         </form>
                     </div>
 
