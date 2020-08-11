5dd14ec75d8a632a401ed73edda0052d048ef7f0
LargonG <LargonG@users.noreply.github.com>
2020-08-11 16:48:41 +1100

    Обновлён вид footer
    Теперь
    1. Он лучше отображается на телефонах
    2. Имеет больше текста (дааа)
    3. Стилизация вынесена в отдельный файл

--- template/footer.php
+++ template/footer.php
@@ -1,52 +1,52 @@
-<style media="screen">
-    .snetlink
-    {
-      width: 50px;
-      height: 50px;
-        transition: 0.3s;
-        margin-right: 1vh;
-    }
-
-    .snetlink:hover
-    {
-        width: 60px;
-        height: 60px;
-    }
-</style>
 <footer class="container text-light mt-4 bg-dark pt-1">
   <div class="row mb-auto">
-    <div class="col-xl-8 col">
-        <b>Мир олимпиадного программирования</b>
+
+    <!-- Текстовая информация -->
+    <div class="col-md-5 col-12 text-center">
+        <span class="font-weight-bold font-italic">МОП - мир олимпиадного программирования</span>
         <div class="mt-2">
-          <a class="text-light" href="#">О проекте</a><br>
-          <a class="text-light" href="#">Наша команда</a><br>
-          <a class="text-light" href="#">Реклама</a>
+          <a class="d-block text-light font-italic" href="#">Пользовательское соглашение</a>
+          <a class="d-block text-light font-italic" href="#">Политика конфиденциальности</a>
+          <a class="d-block text-light font-italic" href="#">О проекте</a>
+          <a class="d-block text-light font-italic" href="#">Наша команда</a>
+          <a class="d-block text-light font-italic" href="#">Связаться с нами</a>
         </div>
-
     </div>
 
-    <div class="col-xl-4 col text-center">
+    <!-- Список соцсетей -->
+    <div class="col-md-4 col-12 mt-md-0 mt-4 mb-md-0 mb-4 text-center ml-auto">
+      
         <b>Мы в соцсетях:</b>
+
         <div class="row justify-content-center mt-1">
-          <a class="col-md-2 col-3 p-0"href="#" title="Вконтакте">
-            <img src=<?php if (!isset($_COOKIE['lighttheme'])) echo "http://prohardinf.ru/imgs/vklink.png"; else echo "http://prohardinf.ru/imgs/dvklink.png"; ?> class="snetlink">
+          
+          <!-- ВК -->
+          <a class="d-flex" href="#" title="Вконтакте">
+            <img src=<?php if (!isset($_COOKIE['lighttheme'])) echo "http://prohardinf.ru/imgs/vklink.png";
+              else echo "http://prohardinf.ru/imgs/dvklink.png"; ?> class="snetlink img-fluid">
           </a>
 
-          <a class="col-md-2 col-3 p-0" href="#" title="Facebook">
-            <img src=<?php if (!isset($_COOKIE['lighttheme'])) echo "http://prohardinf.ru/imgs/fblink.png"; else echo "http://prohardinf.ru/imgs/dfblink.png"; ?> class="snetlink">
+          <!-- Зачем на facebook? -->
+          <a class="d-flex" href="#" title="Facebook">
+            <img src=<?php if (!isset($_COOKIE['lighttheme'])) echo "http://prohardinf.ru/imgs/fblink.png";
+              else echo "http://prohardinf.ru/imgs/dfblink.png"; ?> class="snetlink img-fluid">
           </a>
-
-          <a class="col-md-2 col-3 p-0" href="#" title="Youtube">
-            <img src=<?php if (!isset($_COOKIE['lighttheme'])) echo "http://prohardinf.ru/imgs/ytlink.png"; else echo "http://prohardinf.ru/imgs/dytlink.png"; ?> class="snetlink">
+          
+          <!-- Youtube -->
+          <a class="d-flex" href="#" title="Youtube">
+            <img src=<?php if (!isset($_COOKIE['lighttheme'])) echo "http://prohardinf.ru/imgs/ytlink.png";
+              else echo "http://prohardinf.ru/imgs/dytlink.png"; ?> class="snetlink img-fluid">
           </a>
+        
         </div>
 
     </div>
-  </div>
-  <div class="row bg-secondary align-items-end mt-2">
-    <div class="col p-2 text-center small font-italic">
+
+    <!-- Надпись снизу -->
+    <div class="col-12 bg-secondary mt-2 p-2 text-center small font-italic align-self-end">
         Мир олимпиадного программирования © 2019, все права защищены.
     </div>
+
   </div>
 
 </footer>
